fae8d46c35c65cc03e2c8852c3107c99056ca4a1
jpacsai <judit.pacsai@gmail.com>
2019-10-09 16:15:41 +0200

    remove console.log

--- src/utils/getSeries.js
+++ src/utils/getSeries.js
@@ -10,7 +10,6 @@ export default async () => {
     const seriesList = await fetchSeries(i);
     list = [list, ...seriesList];
   }
-  console.log(list);
   return list;
 }
 
@@ -19,5 +18,3 @@ const fetchSeries = async (offset) => {
   const { data } = await response.json();
   return data.results;
 }
-
-// https://gateway.marvel.com/v1/public/characters?ts=1&apikey=3a6bbe839d07c15e01996061acd6a121&hash=ac4ce3232a11197ececb965dcc3799d1&limit=100
