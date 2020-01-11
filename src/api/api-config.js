47b2ecea295414771d3ebfffb2b8ad4fc653d46a
Your Name <arteevraina@gmail.com>
2020-01-11 23:18:06 +0530

    api key

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-  marvelapikey: "3a6bbe839d07c15e01996061acd6a121",
+  marvelapikey: process.env.REACT_APP_API_KEY,
   marvelapihash: "ac4ce3232a11197ececb965dcc3799d1",
   timestamp: 1,
   perPage: 100
